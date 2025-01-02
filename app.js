5f589e30681de751d08d9dc99b82e717da1671c7
AmiraAHady <amirasgithub@gmail.com>
2025-01-02 11:59:30 +0200

    change app in main

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-myfun=(x,y)=>x+y
-console.log('hello');
+let a1 = [12, 45, 7, 32];
+let max = Math.max(...a);
+let min = Math.min(...a);
+
+let a = [9, 3, 5, 1];
+a.sort((x, y) => x - y); // Ascending
+console.log(a);
+
+a.sort((x, y) => y - x); // Descending
+console.log(a);
